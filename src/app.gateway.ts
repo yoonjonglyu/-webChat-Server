75085ef6a80aafead5127979e3f241e7a2ff3979
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-20 18:40:13 +0900

    feat: SOCKET - INIT & JOIN & SEND 초기 이벤트

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,39 +1,53 @@
 /* eslint-disable prettier/prettier */
 import { Logger } from '@nestjs/common';
 import {
-    MessageBody,
-    OnGatewayConnection,
-    OnGatewayDisconnect,
-    OnGatewayInit,
-    SubscribeMessage,
-    WebSocketGateway,
-    WebSocketServer,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway(80, {
-    transports: ['websocket'],
-    namespace: 'webChat',
+@WebSocketGateway(7778, {
+  transports: ['websocket'],
+  namespace: 'webChat',
 })
 export class AppGateway
-    implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    constructor() { }
-    @WebSocketServer() server: Server;
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
 
-    private logger: Logger = new Logger('AppGateway');
-    @SubscribeMessage('events')
-    handleEvent(@MessageBody() data: string): string {
-        return data;
-    }
-    afterInit(server: Server) {
-        this.logger.log('init');
-    }
-    handleDisconnect(client: Socket) {
-        this.logger.log(`Client Disconnected : ${client.id}`);
-    }
+  @WebSocketServer() server: Server;
 
-    handleConnection(client: Socket, ...args: any[]) {
-        this.logger.log(`Client Connected : ${client.id}`);
-    }
+  private logger: Logger = new Logger('AppGateway');
+  @SubscribeMessage('init')
+  handleEvent(@MessageBody() data: string) {
+    this.logger.log(data);
+  }
+  @SubscribeMessage('join')
+  handleJoin(@MessageBody() data: string) {
+    this.logger.log(data);
+    this.server.socketsJoin(data);
+  }
+  @SubscribeMessage('send')
+  handleSend(@MessageBody() data: string) {
+    this.logger.log(data);
+    this.server.emit('receive', data);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('init');
+  }
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client Disconnected : ${client.id}`);
+  }
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client Connected : ${client.id}`);
+    client.join('#1');
+    client.emit('room', Array.from(client.rooms));
+  }
 }
